ba83e13140025e553ab3c465253cee61588e8f06
bortoliotavio <37578725+bortoliotavio@users.noreply.github.com>
2018-04-14 16:56:57 -0300

    Update Tiangulo.py

--- Tiangulo.py
+++ Tiangulo.py
@@ -1,7 +1,8 @@
 hashtag = 1
 n = int(input('Digite quantas linhas voce quer que seu triangulo: (linhas <=25)  '))
 n += 1
+f = (n * 2) - 1
 while (n != 1):
-    print('{}'.format(('#' * hashtag).center(50)))
+    print('{}'.format(('#' * hashtag).center(f)))
     hashtag += 2
     n -= 1
