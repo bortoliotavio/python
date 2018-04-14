0e4864c2a400721f45be8bbdf4ac1cf983f417f2
bortoliotavio <37578725+bortoliotavio@users.noreply.github.com>
2018-04-14 12:04:11 -0300

    Add files via upload

---
+++ exercicios2.py
@@ -0,0 +1,15 @@
+def bissexto():
+    ano = int(input('Digite o ano:'))
+
+    if ano % 4 == 0:
+        if ano % 100 == 0:
+            if ano % 400 == 0:
+                print('É bissexto')
+            else:
+                print('Não é bissexto')
+        else:
+            print('É bissexto')
+    else:
+        print('Não é bissexto')
+
+bissexto()
